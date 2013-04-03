483041a1d39a77ca892479d902e71ed1b255bff6
Kim Pepper <kim@pepper.id.au>
2013-04-04 10:26:26 +1100

    Ran rspec-puppet-init to create correct symlinks

--- spec/classes/picasa_spec.rb
+++ spec/classes/picasa_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'picasa' do
   
-  it { should contain_class('picasa' )}
+  it { should contain_class('picasa')}
   it { should contain_package('Picasa').with_provider('appdmg') }
   it { should contain_package('Picasa').with_source('http://dl.google.com/photos/picasamac39.dmg') }
 
